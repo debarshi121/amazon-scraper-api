a20ff7baed86b967e03ea639d73b21d01377dae8
Debarshi <debarshi121@gmail.com>
2023-03-18 18:10:30 +0530

    Product

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3005;
 
 const amazonScraper = require("amazon-buddy");
 
@@ -24,8 +24,17 @@ app.get(`/products`, async (req, res, next) => {
 
 app.get(`/products/:asin`, async (req, res, next) => {
 	try {
-		const product = await amazonScraper.asin({ asin: 'B078T6991W' });
-		res.status(200).json({ product });
+		const product = await amazonScraper.asin({ asin: req.params.asin, country: "IN" });
+		res.status(200).json(product);
+	} catch (error) {
+		res.status(500).json({ error });
+	}
+});
+
+app.get(`/products/:asin/reviews`, async (req, res, next) => {
+	try {
+		const reviews = await amazonScraper.reviews({ asin: req.params.asin, number: 50, country: "IN" });
+		res.status(200).json(reviews);
 	} catch (error) {
 		res.status(500).json({ error });
 	}
